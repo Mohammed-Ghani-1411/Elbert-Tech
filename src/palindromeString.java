d9a1e3c2dcfcf7786472a8f6c773973e352135af
MohammadAbdulGhani-sudo <ghanimohd147@gmail.com>
2025-11-06 00:12:28 +0530

    Initial commit

---
+++ src/palindromeString.java
@@ -0,0 +1,18 @@
+public class palindromeString {
+    //check if a string is palindrome
+
+    public static void main(String args[]) {
+        String str = "Mohammed";
+        String rev = "";
+        for (int i = 0; i < str.length(); i++) {
+            rev = str.charAt(i) + rev;
+        }
+        if (rev.equals(str)) {
+            System.out.println(str +
+                    " is a Palindrome");
+        } else {
+            System.out.println(str +
+                    " is not a Palindrome");
+        }
+    }
+}
